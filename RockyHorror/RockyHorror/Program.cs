e784782f74db33ec714c9adab6bde9cac2edb2da
JimmyHsu2 <Smissle4802@gmail.com>
2022-12-12 16:24:00 +0800

    Fix namespace

--- RockyHorror/RockyHorror/Program.cs
+++ RockyHorror/RockyHorror/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+using System.Collections.Generic;
+namespace RockyHorror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
