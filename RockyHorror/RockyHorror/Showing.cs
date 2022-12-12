d6de6465c538ecaab6f7292183150dbb893a43d8
JimmyHsu2 <Smissle4802@gmail.com>
2022-12-12 16:18:35 +0800

    Add Showing class

---
+++ RockyHorror/RockyHorror/Showing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockyHorror
+{
+    public class Showing
+    {
+        public string Location { get; set; }
+
+        public int NumberOfSeats { get; set; }
+
+        public List<DateTime> ShowTimes { get; set; }
+    }
+}
